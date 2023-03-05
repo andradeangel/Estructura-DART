287c7fae57c5e542cfc5b6a740d9e19241560f29
Angel <elfoldine@gmail.com>
2023-03-05 00:08:33 -0400

    Iniciando el repositorio para estructura Dart

---
+++ index.dart
@@ -0,0 +1,32 @@
+void main() {	  				//VOID(VACIO) + MAIN(PRINCIPAL) INICIO DEL PROGRAMA DART
+
+    print("Hello Wolrd!");	 	//PRINT("AQUI SE IMPRIME TODO EN CONSOLA")
+
+////////////////////////////////////////////////////////////////////////////////////
+
+    bool estaSoleado = true;   	//TIPO DE DATO LOGICO (SUS VALORES SON TRUE Y FALSE)
+    int edad = 24; 				//TIPO DE DATO ENTERO
+    double estatura = 1.70;    	//TIPO DE DATO DECIMAL
+    num numero = 5;             //TIPO DE DATO NUMERICO (PUEDE REASIGNARSE DE UN VALOR ENTERO A UN VALOR DECIMAL)
+    String pais = "Bolivia";	//TIPO DE DATO CADENA DE TEXTO NO PRIMITIVO (ACEPTA NULOS Y ALFANUMERICOS)
+    List <String> nombres = ["Angel", "Auron", "Dalex"]; //TIPO DE DATO DE COLECCION (ARRAY), PERMITE MUCHOS VALORES DEL MISMO TIPO EN UNA MISMA VARIABLE
+    var edades = [3, 8, 10];    //TIPO DE DATO DE COLECCION (ARRAY) ES IGUAL QUE LIST PERO IDENTIFICA EL TIPO DE DATO QUE LE ESTAN DANDO Y SE PUEDE RESESCRIBIR VALORES
+    final nombre = "Luis"; 		//TIPO DE DATO QUE NUNCA CAMBIARA DE VALOR Y ENTIENDE EL TIPO DE DATO QUE SE LE ESTA DANDO
+    const nombre2 = "Angel";	//TIPO DE DATO QUE NUNCA CAMBIA DE VALOR PERO A DIFERENCIA DEL FINAL EL CONST TIENE QUE TENER SU VALOR DESDE EL PRINCIPIO
+ 	dynamic edad = 10;			//TIPO DE DATO QUE ENTIENDE EL TIPO DE DATO PERO ESTA SE PUEDE REESCRIBIR EL VALOR COMO TAMBIEN EL TIPO DE DATO
+
+////////////////////////////////////////////////////////////////////////////////////
+
+    //  +   SUMA
+    //  -   RESTA
+    //  *   MULTIPLICACION
+    //  /   DIVISION
+    //  %   MODULO
+    //  ==  IGUAL QUE
+    //  !=  DISTINTO QUE
+    //  >   MAYOR QUE
+    //  <   MENOR QUE
+    //  >=  MAYOR O IGUAL QUE
+    //  <=  MENOR O IGUAL QUE
+    
+}
